ab97a21ba39af96935151ea61ce48950db8f7f32
Rafa <rafael.moraes@drmais.com.br>
2026-08-09 17:25:58 -0300

    bobage

--- src/main/java/studdy/example/demo/DemoApplication.java
+++ src/main/java/studdy/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Ola, mundo!");
+		System.out.println("Servidor subiu, só n pode cair !");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
