bcaf83d66aff61f5bc20296afd536a04182ed88f
SaraMJaber <sarajaber1c@gmail.com>
2026-08-18 17:18:12 -0300

    Update Simulator Response
    Atualização no cód. Simulator Response(iddisciplina)

--- src/main/java/studdy/example/demo/simulatornotes/dto/SimulatorResponse.java
+++ src/main/java/studdy/example/demo/simulatornotes/dto/SimulatorResponse.java
@@ -1,11 +1,8 @@
 package studdy.example.demo.simulatornotes.dto;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 public record SimulatorResponse(
-
-        UUID disciplineId,
 
         BigDecimal currentAverage,
 
